c439f9d055cd7405673a9db661f97020dab0388d
Trevor <tismetrm@gmail.com>
2019-11-22 15:45:03 -0800

    Add new patient field to the response. Convert true or false to equal yes in the response.

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,18 @@
 // Business Logic (export items in this file. Import jQuery here if needed)
+
+// 
+// export newPatientsConverter(boolean){
+//   if (boolean === true){
+//     return "YES";
+//   }
+//   else {
+//     return "Currently Full."
+//   }
+// }
+//
+//
+// let newPatients = profile.accepts_new_patients;
+// console.log(newPatients);
+// newPatients = newPatientsConverter(newPatients);
+//
+// import { newPatientsConverter } from 'scripts';
