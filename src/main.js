4c61a9c92029038a283c386de82007c7ea8b8740
Trevor <tismetrm@gmail.com>
2019-11-22 11:52:46 -0800

    Add code to connect to Better Doctor API. Begin structuring UI.

--- src/main.js
+++ src/main.js
@@ -6,9 +6,47 @@ import './styles.css';
 
 // import { Example, anotherExample } from './scripts';
 
-// UI Logic
+const API_KEY = "44daf8c9eb3e172b888f7b46e3411e9e"; // user_key=API_KEY (parameter)
+const API_Parameter = "user_key";  //
+const endPoint = "https://api.betterdoctor.com/2016-03-01/doctors?";
+
+
+
+
+
 
 
 $(document).ready(function() {
+
+  $('#find-doctor').click(function() {
+
+    let city = $("#city-input").val();
+    let condition = $("#condition-input").val();
+    let request = new XMLHttpRequest();
+
+    let url = `${endPoint}query=${condition}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${API_KEY}`;
+
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+
+
+
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    const getElements = function(response) {
+      let doctorInfo = response.data;
+
+      $(".display-div").show();
+      $('#display-results').empty().text(doctorInfo);
+    };
+
+  });
 
 });
